0d12475dbc6ab16bfd4a8c47ded4995485d1bcdd
aprili1u <44086493+aprili1u@users.noreply.github.com>
2020-12-30 13:04:38 +0100

    Simple bash script to test
    sh: 2 node
    py: print(hello world)

---
+++ submit_node_multiprocessing.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+#SBATCH -o ./job.out.%j
+#SBATCH -e ./job.err.%j
+#SBATCH -D ./
+#SBATCH -J PYTHON_MP
+#SBATCH --mail-type=none
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1   # only start 1 task via srun because Python multiprocessing starts more tasks internally
+#SBATCH --cpus-per-task=2    # assign all the cores to that first task to make room for Python's multiprocessing tasks
+#SBATCH --time=00:01:00
+#SBATCH --mail-user cecile.aprili7@etu.univ-lorraine.fr
+
+module purge
+module load anaconda/3
+
+# avoid overbooking of the cores which might occur via NumPy/MKL threading
+export OMP_NUM_THREADS=1
+
+srun python ./python_multiprocessing.py $SLURM_CPUS_PER_TASK
