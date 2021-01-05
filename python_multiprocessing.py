6fa2995aec8a7ddfb507457ca1e3aae41859f100
aprili1u <cecile.aprili7@etu.univ-lorraine.fr>
2021-01-05 11:54:37 +0100

    back to the original files

--- python_multiprocessing.py
+++ python_multiprocessing.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 
-# import os, sys
-# import multiprocessing as mp
+import os, sys
+import multiprocessing as mp
 
-def my_f():
+def my_f(x):
   """Function to be run in parallel.
   """
-#  print("{} : {}".format(x, os.getpid())) 
-  print("Hello Word")
+  print("{} : {}".format(x, os.getpid()))
 
 # set the number of processes to be used
 #np = mp.cpu_count()   # ... as detected by multiprocessing
-# np = int(sys.argv[1])   # ... as passed in via the command line
+np = int(sys.argv[1])   # ... as passed in via the command line
 
-# with mp.Pool(np) as p:
-#   p.map(my_f, range(np))
-
-my_f()
+with mp.Pool(np) as p:
+  p.map(my_f, range(np))
--- submit_node_multiprocessing.sh
+++ submit_node_multiprocessing.sh
@@ -7,10 +7,13 @@
 #SBATCH --mail-type=none
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1   # only start 1 task via srun because Python multiprocessing starts more tasks internally
-#SBATCH --cpus-per-task=2    # assign all the cores to that first task to make room for Python's multiprocessing tasks
-#SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=32    # assign all the cores to that first task to make room for Python's multiprocessing tasks
+#SBATCH --time=00:01:00
 
 module purge
 module load anaconda/3
 
-srun python ./python_multiprocessing.py
+# avoid overbooking of the cores which might occur via NumPy/MKL threading
+export OMP_NUM_THREADS=1
+
+srun python ./python_multiprocessing.py $SLURM_CPUS_PER_TASK
