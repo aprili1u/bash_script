2c963af4e95274e8d86a56e37e48b0bba53d4ad1
aprili1u <cecile.aprili7@etu.univ-lorraine.fr>
2021-01-05 11:27:42 +0100

    python file is just a print hello world

--- python_multiprocessing.py
+++ python_multiprocessing.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
-import os, sys
-import multiprocessing as mp
+# import os, sys
+# import multiprocessing as mp
 
 def my_f(x):
   """Function to be run in parallel.
   """
-  print("{} : {}".format(x, os.getpid())) 
+#  print("{} : {}".format(x, os.getpid())) 
   print("Hello Word")
 
 # set the number of processes to be used
 #np = mp.cpu_count()   # ... as detected by multiprocessing
-np = int(sys.argv[1])   # ... as passed in via the command line
+# np = int(sys.argv[1])   # ... as passed in via the command line
 
-with mp.Pool(np) as p:
-  p.map(my_f, range(np))
+# with mp.Pool(np) as p:
+#   p.map(my_f, range(np))
--- submit_node_multiprocessing.sh
+++ submit_node_multiprocessing.sh
@@ -9,7 +9,6 @@
 #SBATCH --ntasks-per-node=1   # only start 1 task via srun because Python multiprocessing starts more tasks internally
 #SBATCH --cpus-per-task=2    # assign all the cores to that first task to make room for Python's multiprocessing tasks
 #SBATCH --time=01:00:00
-#SBATCH --mail-user cecile.aprili7@etu.univ-lorraine.fr
 
 module purge
 module load anaconda/3
