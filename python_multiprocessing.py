8471480ceaa8f1bbcfc551834127f92a01ccddd0
aprili1u <cecile.aprili7@etu.univ-lorraine.fr>
2021-01-05 12:21:21 +0100

    change in python file

--- python_multiprocessing.py
+++ python_multiprocessing.py
@@ -1,16 +1,3 @@
 #!/usr/bin/env python
 
-import os, sys
-import multiprocessing as mp
-
-def my_f(x):
-  """Function to be run in parallel.
-  """
-  print("{} : {}".format(x, os.getpid()))
-
-# set the number of processes to be used
-#np = mp.cpu_count()   # ... as detected by multiprocessing
-np = int(sys.argv[1])   # ... as passed in via the command line
-
-with mp.Pool(np) as p:
-  p.map(my_f, range(np))
+print('Hello World')
--- submit_node_multiprocessing.sh
+++ submit_node_multiprocessing.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-type=none
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1   # only start 1 task via srun because Python multiprocessing starts more tasks internally
-#SBATCH --cpus-per-task=32    # assign all the cores to that first task to make room for Python's multiprocessing tasks
+#SBATCH --cpus-per-task=1    # assign all the cores to that first task to make room for Python's multiprocessing tasks
 #SBATCH --time=00:01:00
 
 module purge
