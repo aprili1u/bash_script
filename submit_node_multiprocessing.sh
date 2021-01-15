5f6c13d7640a6441c3654149d8b76cfba922fbe8
aprili1u <cecile.aprili7@etu.univ-lorraine.fr>
2021-01-15 16:49:42 +0100

    change

--- submit_node_multiprocessing.sh
+++ submit_node_multiprocessing.sh
@@ -8,6 +8,7 @@
 #SBATCH --ntasks-per-node=1   # only start 1 task via srun because Python multiprocessing starts more tasks internally
 #SBATCH --cpus-per-task=2    # assign all the cores to that first task to make room for Python's multiprocessing tasks
 #SBATCH --time=00:10:00
+#SBATCH --mem=128G
 
 module purge
 module load anaconda/3
