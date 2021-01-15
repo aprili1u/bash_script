b8fba6616de7159b5a9ff537230b20e773365331
aprili1u <cecile.aprili7@etu.univ-lorraine.fr>
2021-01-15 16:44:09 +0100

    change

--- submit_node_multiprocessing.sh
+++ submit_node_multiprocessing.sh
@@ -8,12 +8,6 @@
 #SBATCH --ntasks-per-node=1   # only start 1 task via srun because Python multiprocessing starts more tasks internally
 #SBATCH --cpus-per-task=2    # assign all the cores to that first task to make room for Python's multiprocessing tasks
 #SBATCH --time=00:10:00
-#SBATCH --mem=128G
-#SBATCH --mail-user cecile.aprili7@etu.univ-lorraine.fr
-#SBATCH --mail-type=begin
-#SBATCH --mail-type=end
-#SBATCH --error=JobName.%J.err
-#SBATCH --output=JobName.%J.out
 
 module purge
 module load anaconda/3
@@ -21,4 +15,6 @@ module load anaconda/3
 # avoid overbooking of the cores which might occur via NumPy/MKL threading
 export OMP_NUM_THREADS=1
 
+echo "starting job ..."
 srun python ./python_multiprocessing.py $SLURM_CPUS_PER_TASK
+echo "... done"
