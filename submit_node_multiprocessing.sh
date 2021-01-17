57dd955c41a64532e3b117207cc413cef36a8f33
aprili1u <cecile.aprili7@etu.univ-lorraine.fr>
2021-01-17 18:52:24 +0100

    added email on the .sh

--- submit_node_multiprocessing.sh
+++ submit_node_multiprocessing.sh
@@ -4,7 +4,7 @@
 #SBATCH -e ./job.err.%j
 #SBATCH -D ./
 #SBATCH -J PYTHON_MP
-#SBATCH --mail-type=none
+#SBATCH --mail-user=cecile.aprili@gmail.com
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1   # only start 1 task via srun because Python multiprocessing starts more tasks internally
 #SBATCH --cpus-per-task=32    # assign all the cores to that first task to make room for Python's multiprocessing tasks
