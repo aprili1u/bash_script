f62658cb5dd74a3fcdb55e9afb4fec8fd57e72c0
aprili1u <cecile.aprili7@etu.univ-lorraine.fr>
2021-01-17 19:11:20 +0100

    eraising unnecessary command lines in .sh

--- submit_node_multiprocessing.sh
+++ submit_node_multiprocessing.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -l
 
-#SBATCH -o ./job.out.%j
-#SBATCH -e ./job.err.%j
 #SBATCH -D ./
 #SBATCH -J PYTHON_MP
 #SBATCH --mail-user=cecile.aprili@gmail.com
@@ -9,7 +7,7 @@
 #SBATCH --mail-type=end
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1   
-#SBATCH --cpus-per-task=32    
+#SBATCH --cpus-per-task=2   
 #SBATCH --time=00:01:00
 #SBATCH --error=JobName.%J.err
 #SBATCH --output=JobName.%J.out
@@ -19,7 +17,4 @@ module load anaconda/3
 
 export OMP_NUM_THREADS=1
 
-echo "stating job ..."
 python ./python_multiprocessing.py $SLURM_CPUS_PER_TASK
-
-echo "...done"
