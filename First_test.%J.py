d37b9e6459ef439609a06da142449715383bf63a
aprili1u <cecile.aprili7@etu.univ-lorraine.fr>
2021-02-01 16:53:29 +0100

    new try with np

--- First_test.%J.py
+++ First_test.%J.py
@@ -22,3 +22,27 @@ tasks of the per-task memory high-water marks; cf. "man sacct"):
 ------------ ---------- ------ ------ ------ ------------ ------------ ---------- -------- 
 3653931       PYTHON_MP      1            20                             00:00:06      1:0 
   
+                                  
+================================= 
+Global information about the job: 
+================================= 
+  
+Job owner: iuyehara(62033)
+Job name:  PYTHON_MP
+Node list: co2637
+Job start: Mon Feb  1 16:52:59 CET 2021
+Job end:   Mon Feb  1 16:53:05 CET 2021
+Work dir:  /u/iuyehara/ceci/bash_script/./
+Command:   /cobra/u/iuyehara/ceci/bash_script/submit_node_multiprocessing.sh
+  
+  
+  
+==========================================================================================
+Information on jobsteps (Note: MaxRSS/AveRSS is the maximum/average over all 
+tasks of the per-task memory high-water marks; cf. "man sacct"): 
+==========================================================================================
+  
+       JobID    JobName NNodes NTasks  NCPUS       MaxRSS       AveRSS    Elapsed ExitCode 
+------------ ---------- ------ ------ ------ ------------ ------------ ---------- -------- 
+3653953       PYTHON_MP      1            20                             00:00:06      0:0 
+  
