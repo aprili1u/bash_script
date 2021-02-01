04560ebae7fa9b5451bea09957f910a12732a15f
aprili1u <cecile.aprili7@etu.univ-lorraine.fr>
2021-02-01 16:48:35 +0100

    ran with .py and .png

---
+++ First_test.%J.py
@@ -0,0 +1,24 @@
+                                  
+================================= 
+Global information about the job: 
+================================= 
+  
+Job owner: iuyehara(62033)
+Job name:  PYTHON_MP
+Node list: co2637
+Job start: Mon Feb  1 16:45:48 CET 2021
+Job end:   Mon Feb  1 16:45:54 CET 2021
+Work dir:  /u/iuyehara/ceci/bash_script/./
+Command:   /cobra/u/iuyehara/ceci/bash_script/submit_node_multiprocessing.sh
+  
+  
+  
+==========================================================================================
+Information on jobsteps (Note: MaxRSS/AveRSS is the maximum/average over all 
+tasks of the per-task memory high-water marks; cf. "man sacct"): 
+==========================================================================================
+  
+       JobID    JobName NNodes NTasks  NCPUS       MaxRSS       AveRSS    Elapsed ExitCode 
+------------ ---------- ------ ------ ------ ------------ ------------ ---------- -------- 
+3653931       PYTHON_MP      1            20                             00:00:06      1:0 
+  
