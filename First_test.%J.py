900e77586729f9ca4847d772f1869718ec03156f
Isaac Kazuo Uyehara <iuyehara@draco01.mpcdf.mpg.de>
2021-02-02 14:14:04 +0100

    test

--- First_test.%J.py
+++ First_test.%J.py
@@ -69,3 +69,27 @@ tasks of the per-task memory high-water marks; cf. "man sacct"):
 ------------ ---------- ------ ------ ------ ------------ ------------ ---------- -------- 
 18772432      PYTHON_MP      1            20                             00:00:09      1:0 
   
+                                  
+================================= 
+Global information about the job: 
+================================= 
+  
+Job owner: iuyehara(62033)
+Job name:  PYTHON_MP
+Node list: draco03
+Job start: Tue Feb  2 14:09:59 CET 2021
+Job end:   Tue Feb  2 14:10:09 CET 2021
+Work dir:  /u/iuyehara/bash_script/./
+Command:   /draco/u/iuyehara/bash_script/submit_node_multiprocessing.sh
+  
+  
+  
+==========================================================================================
+Information on jobsteps (Note: MaxRSS/AveRSS is the maximum/average over all 
+tasks of the per-task memory high-water marks; cf. "man sacct"): 
+==========================================================================================
+  
+       JobID    JobName NNodes NTasks  NCPUS       MaxRSS       AveRSS    Elapsed ExitCode 
+------------ ---------- ------ ------ ------ ------------ ------------ ---------- -------- 
+18772500      PYTHON_MP      1            20                             00:00:10      0:0 
+  
