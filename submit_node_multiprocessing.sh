28acf9f27fc627d02dda0c4ed5e64af6b3a6a1d4
aprili1u <cecile.aprili7@etu.univ-lorraine.fr>
2021-02-02 19:20:10 +0100

    memo changed

--- submit_node_multiprocessing.sh
+++ submit_node_multiprocessing.sh
@@ -11,7 +11,7 @@
 #SBATCH --ntasks-per-node=1   
 #SBATCH --cpus-per-task=10
 #SBATCH --time=02:00:00
-#SBATCH --mem=142G
+#SBATCH --mem=42G
 #SBATCH --error=First_test.%J.err
 #SBATCH --output=First_test.%J.py
 
