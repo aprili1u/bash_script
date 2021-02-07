a167b4a1a380628c7243c1644367e5071cf36920
aprili1u <cecile.aprili7@etu.univ-lorraine.fr>
2021-02-07 16:50:00 +0100

    results when read from file

---
+++ memo_test.%J.py
@@ -0,0 +1,24 @@
+                                  
+================================= 
+Global information about the job: 
+================================= 
+  
+Job owner: iuyehara(62033)
+Job name:  PYTHON_MP
+Node list: co2639
+Job start: Sun Feb  7 14:28:17 CET 2021
+Job end:   Sun Feb  7 16:42:31 CET 2021
+Work dir:  /u/iuyehara/ceci/bash_script/./
+Command:   /cobra/u/iuyehara/ceci/bash_script/submit_node_multiprocessing.sh
+  
+  
+  
+==========================================================================================
+Information on jobsteps (Note: MaxRSS/AveRSS is the maximum/average over all 
+tasks of the per-task memory high-water marks; cf. "man sacct"): 
+==========================================================================================
+  
+       JobID    JobName NNodes NTasks  NCPUS       MaxRSS       AveRSS    Elapsed ExitCode 
+------------ ---------- ------ ------ ------ ------------ ------------ ---------- -------- 
+3675555       PYTHON_MP      1             2                             02:14:14      0:0 
+  
