3cb06e2d0d2252358b63efd6afa07f8e06f50b91
aprili1u <cecile.aprili7@etu.univ-lorraine.fr>
2021-02-12 14:14:19 +0100

    test with everything locked

--- memo_test.%J.py
+++ memo_test.%J.py
@@ -118,3 +118,27 @@ tasks of the per-task memory high-water marks; cf. "man sacct"):
 ------------ ---------- ------ ------ ------ ------------ ------------ ---------- -------- 
 3679752       PYTHON_MP      1            32                             08:45:22      0:0 
   
+                                  
+================================= 
+Global information about the job: 
+================================= 
+  
+Job owner: iuyehara(62033)
+Job name:  PYTHON_MP
+Node list: co2641
+Job start: Wed Feb 10 11:37:40 CET 2021
+Job end:   Thu Feb 11 11:37:42 CET 2021
+Work dir:  /u/iuyehara/ceci/bash_script/./
+Command:   /cobra/u/iuyehara/ceci/bash_script/submit_node_multiprocessing.sh
+  
+  
+  
+==========================================================================================
+Information on jobsteps (Note: MaxRSS/AveRSS is the maximum/average over all 
+tasks of the per-task memory high-water marks; cf. "man sacct"): 
+==========================================================================================
+  
+       JobID    JobName NNodes NTasks  NCPUS       MaxRSS       AveRSS    Elapsed ExitCode 
+------------ ---------- ------ ------ ------ ------------ ------------ ---------- -------- 
+3685019       PYTHON_MP      1            32                           1-00:00:02      0:0 
+  
