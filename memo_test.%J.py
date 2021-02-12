94c4687af3d8222ac7f8385f36124e44878ff05b
aprili1u <cecile.aprili7@etu.univ-lorraine.fr>
2021-02-12 16:11:15 +0100

    results M1 and M3\Smallworld

---
+++ memo_test.%J.py
@@ -0,0 +1,48 @@
+                                  
+================================= 
+Global information about the job: 
+================================= 
+  
+Job owner: iuyehara(62033)
+Job name:  PYTHON_MP
+Node list: co3631
+Job start: Fri Feb 12 16:00:48 CET 2021
+Job end:   Fri Feb 12 16:02:54 CET 2021
+Work dir:  /u/iuyehara/ceci/bash_script/./
+Command:   /cobra/u/iuyehara/ceci/bash_script/submit_node_multiprocessing.sh
+  
+  
+  
+==========================================================================================
+Information on jobsteps (Note: MaxRSS/AveRSS is the maximum/average over all 
+tasks of the per-task memory high-water marks; cf. "man sacct"): 
+==========================================================================================
+  
+       JobID    JobName NNodes NTasks  NCPUS       MaxRSS       AveRSS    Elapsed ExitCode 
+------------ ---------- ------ ------ ------ ------------ ------------ ---------- -------- 
+3694375       PYTHON_MP      1            80                             00:02:06      0:0 
+  
+                                  
+================================= 
+Global information about the job: 
+================================= 
+  
+Job owner: iuyehara(62033)
+Job name:  PYTHON_MP
+Node list: co3631
+Job start: Fri Feb 12 16:03:05 CET 2021
+Job end:   Fri Feb 12 16:05:14 CET 2021
+Work dir:  /u/iuyehara/ceci/bash_script/./
+Command:   /cobra/u/iuyehara/ceci/bash_script/submit_node_multiprocessing.sh
+  
+  
+  
+==========================================================================================
+Information on jobsteps (Note: MaxRSS/AveRSS is the maximum/average over all 
+tasks of the per-task memory high-water marks; cf. "man sacct"): 
+==========================================================================================
+  
+       JobID    JobName NNodes NTasks  NCPUS       MaxRSS       AveRSS    Elapsed ExitCode 
+------------ ---------- ------ ------ ------ ------------ ------------ ---------- -------- 
+3694530       PYTHON_MP      1            80                             00:02:09      0:0 
+  
