6db1744c466210c8a90bd76cd99d76d256d955a8
aprili1u <cecile.aprili7@etu.univ-lorraine.fr>
2021-03-16 19:31:29 +0100

    data of frist run

---
+++ memo_test.%J.py
@@ -0,0 +1,24 @@
+                                  
+================================= 
+Global information about the job: 
+================================= 
+  
+Job owner: iuyehara(62033)
+Job name:  PYTHON_MP
+Node list: co1282
+Job start: Sun Mar 14 19:25:35 CET 2021
+Job end:   Mon Mar 15 19:25:37 CET 2021
+Work dir:  /u/iuyehara/ceci/bash_script/./
+Command:   /cobra/u/iuyehara/ceci/bash_script/submit_node_multiprocessing.sh
+  
+  
+  
+==========================================================================================
+Information on jobsteps (Note: MaxRSS/AveRSS is the maximum/average over all 
+tasks of the per-task memory high-water marks; cf. "man sacct"): 
+==========================================================================================
+  
+       JobID    JobName NNodes NTasks  NCPUS       MaxRSS       AveRSS    Elapsed ExitCode 
+------------ ---------- ------ ------ ------ ------------ ------------ ---------- -------- 
+3807797       PYTHON_MP      1            80                           1-00:00:02      0:0 
+  
---
+++ memo_test.3807797.py
@@ -0,0 +1,1 @@
+['ID', 'trial', 'generations', 'num_nodes', 'interactions_per_node', 'interaction_partners', 'memory_cost', 'initial_memory_poisson', 'Win_fight', 'Lose_fight', 'Hawk_D', 'Dove_H', 'DoveDove']
